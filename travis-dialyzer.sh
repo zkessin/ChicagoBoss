93c3f558f5a7dedc49e513487e45cc82950ac5a6
Zachary Kessin <zkessin@gmail.com>
2013-12-11 09:54:39 +0200

    for some reason riak creates a few beam files that don't have debug info set, so the script removes them before adding all the deps to the plt files.
    This should do it

--- travis-dialyzer.sh
+++ travis-dialyzer.sh
@@ -2,14 +2,15 @@
 
 
 PLT=plt/cb.plt
-echo $PLT
+echo "PLT File $PLT"
 export PATH=$PATH:/usr/local/bin:/usr/bin
+echo "Building PLT, may take a few minutes"
 dialyzer  --build_plt --apps kernel stdlib mnesia  inets ssl crypto \
        erts public_key runtime_tools compiler asn1 hipe gs\
        syntax_tools edoc xmerl \
        --statistics\
        --output_plt $PLT
-rm -f deos/riak_*/ebin/*_pb.beam  
+rm -f deps/riak_*/ebin/*_pb.beam  
 echo "********************************************************************************"
 dialyzer --add_to_plt deps/*/ebin						--plt $PLT
 echo "********************************************************************************"
