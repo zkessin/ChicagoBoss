d036c25a553f098dc8f5e2a31fff05ed108a99d5
Zachary Kessin <zkessin@gmail.com>
2014-01-26 14:29:34 +0200

    add a test runner

---
+++ test/prop_runner.erl
@@ -0,0 +1,24 @@
+-module(prop_runner).
+-include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include("../src/boss/boss_web.hrl").
+
+-compile(export_all).
+
+
+
+gen(Tests, TModule) ->
+    gen(Tests, TModule, 100, 12).
+gen(Tests, TModule,Count,PCount) ->
+    {inparallel, PCount,
+     begin
+         [case Test of
+              {Funct, Arity} ->
+                  ?_assert(proper:check_spec({TModule, Funct, Arity},
+                                            [{to_file, user}, Count]));
+                  
+              F when is_function(F,0) ->
+                  ?_assert(proper:quickcheck(F(), 
+                                             [{to_file, user},Count]))
+          end||Test <-Tests]
+     end}.
