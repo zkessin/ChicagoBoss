2526116badbaad347d523a517038835eea81fcc4
Zachary Kessin <zkessin@gmail.com>
2014-02-03 14:54:56 +0200

    lets see if this works

--- travis-dialyzer.sh
+++ travis-dialyzer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
+mkdir plt
 PLT=plt/cb-$RANDOM.plt
-echo "PLT File $PLT"
+echo  "PLT File $PLT"
 export PATH=$PATH:/usr/local/bin:/usr/bin
-echo "Building PLT, may take a few minutes"
+
 dialyzer  --build_plt --apps kernel stdlib\
-       --output_plt $PLT > /dev/null
+          --output_plt $PLT > /dev/null
 for app in  mnesia  inets ssl crypto \
        erts public_key runtime_tools compiler asn1 hipe\
        syntax_tools 
@@ -22,11 +22,8 @@ do
    echo "Adding $app"
    dialyzer --add_to_plt --apps deps/$app \
        --plt $PLT > /dev/null
-
-
 done
 echo "********************************************************************************"
-echo ""
 
 dialyzer	ebin/		\
     -Werror_handling		\
@@ -34,7 +31,6 @@ dialyzer	ebin/		\
     -Wrace_conditions		\
     --statistics -n             \
     --fullpath			\
-    -n                          \
     --plt $PLT #  -Wunmatched_returns 
 #  
 
